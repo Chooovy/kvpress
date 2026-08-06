9f240bf6ce70fc52c0983870c2053f4eca3fd8d0
/bin/eash <marcushaogu@tencent.com>
2026-08-06 21:09:11 +0800

    hf_download

---
+++ hf_download_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+export HF_TOKEN="${HF_TOKEN:-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}"
+
+DATASET_ID="${DATASET_ID:-sagels/longmino_256k_filtered}"
+LOCAL_DIR="${LOCAL_DIR:-/apdcephfs_zw31/share_303843174/user/marcushaogu/datasets/longmino_256k_filtered}"
+
+hf download \
+  "$DATASET_ID" \
+  --repo-type dataset \
+  --local-dir "$LOCAL_DIR" \
+  --token "$HF_TOKEN" \
